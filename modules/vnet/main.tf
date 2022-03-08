ac71800764d650ca76154aec146130cd762c9319
bruno.silva <john.doe@contoso.com>
2022-03-08 16:01:45 -0300

    added subnets in vnet module

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -8,3 +8,11 @@ resource "azurerm_virtual_network" "vnet" {
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
 }
+
+resource "azurerm_subnet" "subnets" {
+  name                 = lookup(element(var.subnets, count.index), "name")
+  count                = length(var.subnets)
+  resource_group_name  = data.azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefix       = lookup(element(var.subnets, count.index), "address_prefix")
+}
--- modules/vnet/variables.tf
+++ modules/vnet/variables.tf
@@ -6,6 +6,20 @@ variable "vnet_address_space" {
   type = list(string)
 }
 
+variable "subnets" {
+  type = list(string)
+  default = [
+    {
+      address_prefix = "10.0.1.0/24"
+      name           = "subnet-1"
+    },
+    {
+      address_prefix = "10.0.2.0/24"
+      name           = "subnet-2"
+    }
+  ]
+}
+
 #variable "vnet_address_space" {
 #  type = string
 #}
